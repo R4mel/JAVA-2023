78bac9bbbef9ba3f16d47b9d377a0dd7b1ee4e3f
Ramel <dev_20@naver.com>
2023-09-18 19:07:00 +0900

    algorithm

--- src/AutoCheck.java
+++ src/AutoCheck.java
@@ -9,15 +9,14 @@ import java.nio.file.*;
 /******************************************************************************
  * class AutoCheck
  ******************************************************************************/
-class AutoCheck
-{
-    private boolean     isTrace;
-    private int         autoCheck;
-    private Scanner     inScan;
-    private String      output;
+class AutoCheck {
+    private boolean isTrace;
+    private int autoCheck;
+    private Scanner inScan;
+    private String output;
     private final String TEST_DIR = "test";
-    private final String OJ_DIR   = "oj";
-    
+    private final String OJ_DIR = "oj";
+
 
     public AutoCheck(int autoCheck, boolean isTrace) {
         this.autoCheck = autoCheck;
@@ -29,28 +28,26 @@ class AutoCheck
         inScan = new Scanner(System.in);
         File testDir = new File(TEST_DIR);
         String fileNames[] = testDir.list();
-        
+
         for (var fn : fileNames) {
             if (!fn.endsWith(".in")) continue;
-            String testName = fn.substring(0, fn.length()-3);
-            String path = TEST_DIR+"/"+testName;
-            String inPath  = path+".in";
-            String outPath = path+".out";
+            String testName = fn.substring(0, fn.length() - 3);
+            String path = TEST_DIR + "/" + testName;
+            String inPath = path + ".in";
+            String outPath = path + ".out";
             try {
                 simulate(inPath);
 
                 if (autoCheck == 1)
                     evaluate(testName, inPath, outPath);
                 else if (autoCheck == 2) {
-                    copyFile( inPath, null,   OJ_DIR);
+                    copyFile(inPath, null, OJ_DIR);
                     copyFile(outPath, output, OJ_DIR);
-                }
-                else if (autoCheck == 3)
+                } else if (autoCheck == 3)
                     copyFile(outPath, output, TEST_DIR);
 
-            } 
-            catch (IOException e) { 
-                System.out.println(e); 
+            } catch (IOException e) {
+                System.out.println(e);
             }
         }
         inScan.close();
@@ -68,33 +65,33 @@ class AutoCheck
         curOut.close(); // outStream.close();
         System.setOut(out);
     }
-    
+
     void evaluate(String prompt, String inPath, String correctPath) throws IOException {
         String correct = Files.readString(Paths.get(correctPath));
         String corStr = correct.replaceAll("\\s", "");
-        String outStr = output .replaceAll("\\s", "");
+        String outStr = output.replaceAll("\\s", "");
         boolean isCorrect = corStr.equals(outStr);
-        System.out.println(prompt+" : "+(isCorrect? "O":"X"));
+        System.out.println(prompt + " : " + (isCorrect ? "O" : "X"));
         System.out.flush();
         if (!isCorrect && isTrace)
             diff(output, correct);
     }
 
     void copyFile(String srcPath, String content, String newDir) throws IOException {
-        String dstPath = srcPath.replace(TEST_DIR+"/", newDir+"/");
-        System.out.println("copy file: "+srcPath+"\t-->    "+dstPath);
+        String dstPath = srcPath.replace(TEST_DIR + "/", newDir + "/");
+        System.out.println("copy file: " + srcPath + "\t-->    " + dstPath);
         if (content == null)
             content = Files.readString(Paths.get(srcPath));
-        Files.writeString( Paths.get(dstPath), content, StandardOpenOption.WRITE, 
-                    StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        Files.writeString(Paths.get(dstPath), content, StandardOpenOption.WRITE,
+                StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 
     void diff(String outputStr, String correctStr) {
-        var outputTR  = new TokenReader(outputStr,  true);
+        var outputTR = new TokenReader(outputStr, true);
         var correctTR = new TokenReader(correctStr, false);
         String outputNext, correctNext;
         while (true) {
-            outputNext  = outputTR.next();
+            outputNext = outputTR.next();
             correctNext = correctTR.next();
             if ((outputNext == null) || (correctNext == null))
                 break;
@@ -102,7 +99,7 @@ class AutoCheck
                 printTraceMessage(correctTR.getLine(), outputNext, correctNext);
                 outputTR.skipCurLine();
                 correctTR.skipCurLine();
-             }
+            }
         }
         if ((outputNext == null) && (correctNext == null))
             return;
@@ -114,9 +111,9 @@ class AutoCheck
 
     void printTraceMessage(String line, String outputToken, String correctToken) {
         System.out.println("^--- [WRONG line]");
-        System.out.println(line+"\n"+"^--- [CORRECT line]");
-        System.out.println("output(\""+outputToken+
-                           "\") != correct(\"" +correctToken+"\")");
+        System.out.println(line + "\n" + "^--- [CORRECT line]");
+        System.out.println("output(\"" + outputToken +
+                "\") != correct(\"" + correctToken + "\")");
         System.out.print("continue[y/n]? ");
         String next = inScan.next();
         if (!next.equals("y") && !next.equals("Y")) {
@@ -130,8 +127,7 @@ class AutoCheck
 /******************************************************************************
  * class TokenReader
  ******************************************************************************/
-class TokenReader
-{
+class TokenReader {
     private String lines[];
     private String line;
     private String tokens[];
@@ -145,26 +141,30 @@ class TokenReader
         nextLine();
     }
 
-    public void skipCurLine() { idx = tokens.length; }
+    public void skipCurLine() {
+        idx = tokens.length;
+    }
 
-    public String getLine() { return line; }
+    public String getLine() {
+        return line;
+    }
 
     public String[] nextLine() {
         idx = 0;
         while (curLine < lines.length) {
-        	line = lines[curLine++];
-        	// 끝의 '\r' 문자 제거
+            line = lines[curLine++];
+            // 끝의 '\r' 문자 제거
             if (printMode) System.out.println(line.stripTrailing());
             if (line.isEmpty()) continue;
             tokens = line.split("\\s");
-            if ((tokens.length == 0) || 
-                ((tokens.length == 1) && (tokens[0].isEmpty()))) 
+            if ((tokens.length == 0) ||
+                    ((tokens.length == 1) && (tokens[0].isEmpty())))
                 continue;
             return tokens;
         }
         return null;
     }
-    
+
     public String next() {
         if ((idx == tokens.length) && (nextLine() == null))
             return null;
@@ -172,7 +172,7 @@ class TokenReader
     }
 
     public void printRemainingLines(String preMsg, String postMsg) {
-        System.out.println(preMsg+" has more lines than "+postMsg+" as follows.");
+        System.out.println(preMsg + " has more lines than " + postMsg + " as follows.");
         System.out.println(line);
         while (nextLine() != null)
             System.out.println(line);
