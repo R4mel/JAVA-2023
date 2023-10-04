aa3438c9a8c00781d4acee44f358b9046933eb21
Ramel <dev_20@naver.com>
2023-10-04 12:03:10 +0900

    fibo

--- src/DP/fibo.java
+++ src/DP/fibo.java
@@ -1,4 +1,14 @@
 package DP;
 
 public class fibo {
+    public static long fibBottomUp(int n) {
+        long[] bottomUp = new long[n + 1];
+        bottomUp[1] = 1;
+        bottomUp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            bottomUp[i] = bottomUp[i - 1] + bottomUp[i - 2];
+        }
+        return bottomUp[n];
+    }
+    // Dynamic Programming, Bottom-up
 }
