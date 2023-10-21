873c6dbbd265c51a120c6c800c6147fd9e2bf68b
Ramel <dev_20@naver.com>
2023-10-21 21:15:02 +0900

    practice

--- src/practice5_1/Main.java
+++ src/practice5_1/Main.java
@@ -6,135 +6,165 @@ class Point {
     private int x, y; // 점의 좌표
 
     public Point(int x, int y) {
-        this.x = x;    this.y = y;
+        this.x = x;
+        this.y = y;
     }
+
     public void display() {  // 좌표 출력
-        System.out.print("("+x+","+y+")");
+        System.out.print("(" + x + "," + y + ")");
+    }
+
+    public void move(int width, int height) { // 좌표 이동
+        x += width;
+        y += height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
 
-// 화면에 그래픽을 그려 주기 위한 인터페이스이다.
-interface Graphics {
+// Shape 추상 클래스 선언
+abstract class Shape {
+    private int thick;       // 선의 두께
+    private String color;    // 선의 색깔
+    public Shape next;       // Lined list에서 다음 shape의 참조자
 
-    // 다음과 같이 선의 양 끝 점 p1과 p2의 좌표를 출력한다.
-    // 예) LinuxGUI: L(10,11)(20,22)
-    // "LinuxGUI"는 이 함수를 구현한 시스템 이름이며, 구현 시스템에 따라 다르다.
+    public Shape(int thick, String color) {
+        next = null;
+        this.thick = thick;
+        this.color = color;
+    }
 
-    void drawLine(Point p1, Point p2);
+    public void display() {  // 그래픽 정보 출력
+        System.out.print("(" + thick + "," + color + ")");
+    }
+
+    public abstract void move(int width, int height); // 좌표 이동
 }
 
-// Window 시스템을 위한 인터페이스이다.
-interface Window {
+class Line extends Shape {
+    private Point p1, p2; // 두 점의 좌표
 
-    // 다음과 같이 생성할 윈도우 창의 대각선 양 끝 점 p1과 p2의 좌표를 출력한다.
-    // 예) LinuxGUI: W(10,11)(20,22)
-    // "LinuxGUI"는 이 함수를 구현한 시스템 이름이며, 구현 시스템에 따라 다르다.
-
-    void createWindow(Point p1, Point p2); // 윈도우 창을 생성해 주는 함수이다.
-}
-
-// Window 창과 그래픽을 지원하는 시스템을 위한 GUI 인터페이스이다.
-// 문제: 이 인터페이스는 기존의 Graphics와 Window 인터페이스를 상속해야 한다.
-interface GUI extends Graphics, Window{ // 두 인터페이스 상속할 것
-
-    // GUI에 새로 추가된 함수임
-    // 다음과 같이 마우스 이벤트가 발생한 좌표를 출력을 한다.
-    // 예) LinuxGUI: E(10,11)
-    // "LinuxGUI"는 이 함수를 구현한 시스템 이름이며, 구현 시스템에 따라 다르다.
-
-    void inputMounseEvent(Point p); // 마우스 이벤트를 받아들이는 함수이다.
-}
-
-class LinuxGUI implements GUI{
-    @Override
-    public void drawLine(Point p1, Point p2) {
-        System.out.print("LinuxGUI: L");
-        p1.display();
-        p2.display();
-        System.out.println();
+    public Line(int thick, String color, Point p1, Point p2) {
+        super(thick, color);
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
     @Override
-    public void createWindow(Point p1, Point p2) {
-        System.out.print("LinuxGUI: W");
-        p1.display();
-        p2.display();
-        System.out.println();
+    public void display() {
+        System.out.print("L");
+        super.display();
+        System.out.print("(" + p1.getX() + "," + p1.getY() + ")");
+        System.out.println("(" + p2.getX() + "," + p2.getY() + ")");
     }
 
     @Override
-    public void inputMounseEvent(Point p) {
-        System.out.print("LinuxGUI: E");
-        p.display();
-        System.out.println();
+    public void move(int width, int height) {
+        p1.move(width, height);
+        p2.move(width, height);
     }
 }
 
-class MSWindowsGUI implements GUI{
-    @Override
-    public void drawLine(Point p1, Point p2) {
-        System.out.print("MSWindowsGUI: L");
-        p1.display();
-        p2.display();
-        System.out.println();
+class Circle extends Shape {
+    private Point center;    // 중심점
+    private int radius;      // 반지름
+
+    public Circle(int thick, String color, Point center, int radius) {
+        super(thick, color);
+        this.center = center;
+        this.radius = radius;
     }
 
     @Override
-    public void createWindow(Point p1, Point p2) {
-        System.out.print("MSWindowsGUI: W");
-        p1.display();
-        p2.display();
-        System.out.println();
+    public void display() {
+        System.out.print("C");
+        super.display();
+        System.out.println("(" + center.getX() + "," + center.getY() + "):" + radius);
     }
 
     @Override
-    public void inputMounseEvent(Point p) {
-        System.out.print("MSWindowsGUI: E");
-        p.display();
-        System.out.println();
+    public void move(int width, int height) {
+        center.move(width, height);
+    }
+}
+
+// add() 함수를 통해 추가된 그래픽 객체들을 linked list로 관리한다.
+class Manager {
+    Shape first, last; // Shape의 linked list의 시작과 끝을 참조함
+
+    public Manager() {
+        first = last = null;
+    }
+
+    public void add(Shape s) {
+        if (first == null) {
+            first = s;
+            last = first;
+        } else {
+            last.next = s;
+            last = s;
+        }
+    }
+
+    public void display() { // linked 리스트의 모든 그래픽 객체를 출력한다.
+        for (Shape s = first; s != null; s = s.next)
+            s.display();
+    }
+
+    // 그래픽 객체의 이동할 width, height는 사용자로부터 직접 입력 받은 후
+    // linked 리스트의 모든 그래픽 객체의 좌표를 width, height 만큼 이동한다.
+    public void move(Scanner in) {
+        System.out.print("width and height to move? ");
+        int width = in.nextInt();
+        int height = in.nextInt();
+        for (Shape s = first; s != null; s = s.next)
+            s.move(width, height);
+        display();
     }
 }
 
 public class Main {
+    // 그래픽 객체를 미리 생성하여 배열에 저장해 둠.
+    static private Shape[] shapes = {
+            new Line(0, "BLACK", new Point(10, 11), new Point(20, 21)),
+            new Line(1, "WHITE", new Point(30, 31), new Point(40, 41)),
+            new Circle(2, "RED", new Point(50, 51), 10),
+            new Circle(3, "RED", new Point(50, 51), 10),
+    };
 
-    static Point p1, p2;
-
-    // 한 점의 좌표를 입력 받은 후 Point 객체를 생성한 후 리턴
-    static Point getPoint(Scanner in) {
-        System.out.print("Point x and y? ");
-        int x = in.nextInt();
-        int y = in.nextInt();
-        return new Point(x, y);
-    }
-
-    static void runGraphis(Graphics g) {
-        g.drawLine(p1, p2);
-    }
-
-    static void runWindow(Window w) {
-        w.createWindow(p1, p2);
-    }
-
-    static void GUIsystem(GUI gui) {
-        runGraphis(gui);
-        runWindow(gui);
-        gui.inputMounseEvent(p1);
-    }
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // 두 점의 좌표를 미리 생성해 둔다.
-        p1 = getPoint(in);
-        p2 = getPoint(in);
-        System.out.println();
+        // Manager의 linked 리스트에 처음으로 삽입할 그래픽 객체 배열 인덱스를 입력 받음
+        System.out.print("begin index(0~4) of shapes array? ");
+        int beg = in.nextInt();
 
-        LinuxGUI linux = new LinuxGUI();
-        GUIsystem(linux);
+        Manager manager = new Manager();
+        // 입력 받은 배열 인덱스(beg)부터 순서적으로 Manager에 추가함
+        for (int count = 0; count < shapes.length; count++, beg = ++beg % 4)
+            manager.add(shapes[beg]);
 
-        MSWindowsGUI ms = new MSWindowsGUI();
-        GUIsystem(ms);
+        manager.display();
 
+        while (true) {
+            System.out.print("\n0.exit 1.display 2.move >> ");
+            int input = in.nextInt(); // 메뉴를 보여 주고 메뉴 항목을 입력 받음
+            if (input == 0)
+                break;
+            switch (input) {
+                case 1:
+                    manager.display();
+                    break;
+                case 2:
+                    manager.move(in);
+                    break;
+            }
+        }
         in.close();
     }
 }
