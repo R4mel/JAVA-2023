a20f33f7fcea726f920b843ed74cfb00d0c22eb7
Ramel <dev_20@naver.com>
2023-10-31 12:56:18 +0900

    programmers Day 2 all clear

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -4,22 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Solution {
-    public static String sol(String my_string, String overwrite_string, int s) {
-        String first = my_string.substring(0, s);
-        String second = my_string.substring(s+ overwrite_string.length());
-        return first + overwrite_string + second;
-    }
-
-    public static void main(String[] args) {
-//        String result = sol("Program29b8UYP", "merS123", 7);
-//        System.out.println(result);
-//        String result2 = sol("He11oWor1d", "lloWorl", 2);
-//        System.out.println(result2);
-        System.out.println(sol("aaaaaa", "bbb", 3));
+class Answer {
+    public int sol(int a, int b, boolean flag) {
+        if(flag){
+            return a + b;
+        }
+        return a - b;
     }
 }
 
-// "aaaaaa", "bbb", 3
 
-// "aaabbb"
+class Solution {
+    public static void main(String[] args) {
+        Answer answer = new Answer();
+    }
+}
