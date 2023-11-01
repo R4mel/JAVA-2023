bd23373dfc78998dec3efe64b31e45955941062e
Ramel <dev_20@naver.com>
2023-11-02 00:03:45 +0900

    programmers

--- src/programmers/Application.java
+++ src/programmers/Application.java
@@ -1,14 +1,24 @@
 package programmers;
 
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        return answer;
+        for(int [] q1: queries){
+            int tmp1 = arr[q1[0]];
+            int tmp2 = arr[q1[1]];
+            arr[q1[0]] = tmp2;
+            arr[q1[1]] = tmp1;
+        }
+        return arr;
     }
 }
 
 class Application {
     public static void main(String[] args) {
         Solution test = new Solution();
+        int [] arr = {0, 1, 2, 3, 4};
+        int [][] queries = {{0, 3},{1, 2},{1, 4}};
+        System.out.println(Arrays.toString(test.solution(arr, queries)));
     }
 }
