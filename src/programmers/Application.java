0ac1538013f8c39d92520685c9e4f107e9a6ea32
Ramel <dev_20@naver.com>
2023-11-01 23:34:14 +0900

    programmers day 5 all clear

---
+++ src/programmers/Application.java
@@ -0,0 +1,14 @@
+package programmers;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+class Application {
+    public static void main(String[] args) {
+        Solution test = new Solution();
+    }
+}
