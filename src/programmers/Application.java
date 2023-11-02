344443efd9a35215c80b0e00c9f06eb834048548
Ramel <dev_20@naver.com>
2023-11-02 23:00:08 +0900

    programmers Day 6 all clear

--- src/programmers/Application.java
+++ src/programmers/Application.java
@@ -1,16 +1,22 @@
 package programmers;
 
 import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for(int [] q1: queries){
-            int tmp1 = arr[q1[0]];
-            int tmp2 = arr[q1[1]];
-            arr[q1[0]] = tmp2;
-            arr[q1[1]] = tmp1;
+        int[] arr1 = new int[queries.length];
+        Arrays.fill(arr1, Integer.MAX_VALUE);
+        for(int i = 0; i < queries.length; i++) { // 쿼리의 길이만큼 반복
+            for (int j = queries[i][0]; j <= queries[i][1]; j++){
+                // 각 쿼리의 s <= i <= e의 조건을 for문을 이용해 충족
+                if( arr[j] > queries[i][2]) { // i > k의 조건
+                    if(arr1[i] > arr[j]) // 가장 작은 arr[i] 조건
+                        arr1[i] = arr[j];
+                }
+            }
+            if(arr1[i] == Integer.MAX_VALUE)
+                arr1[i] = -1;
         }
-        return arr;
+        return arr1;
     }
 }
 
