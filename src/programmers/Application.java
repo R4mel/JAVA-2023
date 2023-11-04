d2437c509ff57fd95d673a180df38534f7e56a6f
Ramel <dev_20@naver.com>
2023-11-04 23:50:39 +0900

    programmers

--- src/programmers/Application.java
+++ src/programmers/Application.java
@@ -4,10 +4,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
+
+        return arr;
+    }
+
+    public void dispPersonInfo(String sname) {
+        char first = sname.charAt(0);
+        char last = sname.charAt(sname.length() - 1);
+        String add = sname.substring(1, sname.length() - 1);
+        sname = last + add + first;
+        System.out.println(sname);
+    }
 }
 
 class Application {
     public static void main(String[] args) {
         Solution test = new Solution();
+        String a = "Gwangju ,Nam-gu , Bongseon-dong 21";
+        a.toLowerCase().replaceAll("-gu", "_gu");
+        String[] b = a.split(",");
+        for(int i=0; i<b.length; i++){
+            System.out.println(b[i].trim());
+        }
+
     }
 }
